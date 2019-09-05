2add5a8a7c0b16e537d410cf3fe033aa9da9061e
Duc Trinh <ducanhtrinh93@gmail.com>
2019-09-05 16:52:37 +0200

    Add trailer link, reviews, similar movies in MovieDetails component

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { API_KEY, BASE_URL, MOVIE_LINK } from '../../api'
 import Header from '../Header'
+import MovieBox from '../MovieBox'
 import './index.css'
 
 const Movie = (props) => {
     const movieId = props.match.params.id
 
     const [movie, setMovieDetails] = useState(null)
+    const [movieReviews, setMovieReviews] = useState(null)
+    const [similarMovies, setSimilarMovies] = useState(null)
+    let movieTrailer
+
+    if (movie) {
+        movieTrailer = movie.videos.results.find(video => video.type === 'Trailer')
+    }
+
     useEffect(() => {
-        fetch(`${BASE_URL}${MOVIE_LINK}/${movieId}?api_key=${API_KEY}`)
+        fetch(`${BASE_URL}${MOVIE_LINK}/${movieId}?api_key=${API_KEY}&append_to_response=videos`)
             .then(response => response.json())
             .then(movie => setMovieDetails(movie))
+
+        fetch(`${BASE_URL}${MOVIE_LINK}/${movieId}/reviews?api_key=${API_KEY}`)
+            .then(response => response.json())
+            .then(reviews => setMovieReviews(reviews.results))
+
+        fetch(`${BASE_URL}${MOVIE_LINK}/${movieId}/similar?api_key=${API_KEY}`)
+            .then(response => response.json())
+            .then(movies => setSimilarMovies(movies.results))
     }, [movieId])
 
-    return (movie &&
-        <div>
+    return (movie
+        ? <div>
             <Header />
             <div className="Movie-wrapper">
                 <div className="movie-content">
@@ -23,6 +40,12 @@ const Movie = (props) => {
                     </div>
                     <div className="movie-data">
                         <h1 className="movie-title">{movie.title}</h1>
+
+                        {movieTrailer &&
+                            <h3><a href={`https://www.youtube.com/watch?v=${movieTrailer.key}`} target={"_blank"}>
+                                Watch trailer
+                            </a></h3>}
+
                         <h3 className="movie-info-title">Overview:</h3>
                         <span className="movie-info">{movie.overview}</span>
                         <h3 className="movie-info-title">Release date:</h3>
@@ -32,11 +55,33 @@ const Movie = (props) => {
                         <h3 className="movie-info-title">Revenue:</h3>
                         <span className="movie-info">${movie.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
                         <h3 className="movie-info-title">Average Rating Score:</h3>
-                        <span className="movie-info-vote_average">{movie.vote_average}</span>
+                        <span className="movie-info">{movie.vote_average}</span>
                     </div>
+                </div>
+
+                <div>
+                    {movieReviews &&
+                        <div className="reviews">
+                            <h3 className="similar-movie-heading">Reviews</h3>
+                            {movieReviews.map(review => <div className="review">
+                                <span className="review-author">{review.author} said:</span>
+                                <span className="review-content">{review.content}</span>
+                            </div>)}
+                        </div>
+                    }
+                </div>
+
+                <div>
+                    {similarMovies &&
+                        <div>
+                            <h3 className="similar-movie-heading">You might be interested in</h3>
+                            <MovieBox movies={similarMovies} />
+                        </div>}
                 </div>
             </div>
         </div>
+
+        : <p>Loading...</p>
     )
 }
 
