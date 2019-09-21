32c596d88911cc2396e1e04400dce41168c8b24d
Duc Trinh <ducanhtrinh93@gmail.com>
2019-09-21 13:15:39 +0200

    Update api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 export const BASE_URL = 'https://api.themoviedb.org/3'
-export const API_KEY = process.env.API_KEY
+export const API_KEY = process.env.REACT_APP_API_KEY
 export const SEARCH_LINK = '/search'
 export const MOVIE_LINK = '/movie'
 export const DISCOVER_LINK = '/discover'
 export const POPULAR_LINK = '/popular'
 export const TOP_RATED_LINK = '/top_rated'
-export const UPCOMING_LINK = '/upcoming'
